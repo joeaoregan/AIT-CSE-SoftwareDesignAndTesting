8be53519d324a632be78d150832340ad1c2dc815
Joe O'Regan <a00258304@student.ait.ie>
2018-07-24 12:43:16 +0100

    Software Development: JUnit

---
+++ src/com/ait/cat/Cat.java
@@ -0,0 +1,42 @@
+/*
+ * Joe O'Regan
+ * A00258304
+ * 24/07/2018
+ * Software Development
+ * Lec8 S40
+ */
+
+package com.ait.cat;
+
+public class Cat {
+	private String name;
+	private int age;
+	private String colour;
+	
+	public Cat(String name, int age, String colour) {
+		this.name = name;
+		this.age = age;
+		this.colour = colour;
+	}
+	
+	public String getName() {
+		return name;
+	}	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+}
---
+++ src/com/ait/test/CatTest.java
@@ -0,0 +1,47 @@
+/*
+ * Joe O'Regan
+ * A00258304
+ * 24/07/2018
+ * Software Development
+ * Lec8 S44
+ */
+
+package com.ait.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.ait.cat.Cat;
+
+class CatTest {
+
+	@Test
+	public void testCatConstructred() {
+		Cat cat = new Cat("Tom", 4, "Black"); // Create Cat object
+		assertEquals("Tom", cat.getName()); // Check the values are equal
+		assertEquals(4, cat.getAge());
+		assertEquals("Black", cat.getColour());
+	}
+	
+	@Test
+	void testCatNameChanged() {
+		Cat cat = new Cat("Tom", 4, "Black");
+		cat.setName("Jerry");
+		assertEquals("Jerry",cat.getName());
+	}
+	
+	@Test
+	void testCatAgeChanged() {
+		Cat cat = new Cat("Tom", 4, "Black");
+		cat.setAge(5);
+		assertEquals(5,cat.getAge());
+	}
+	
+	@Test
+	void testCatColourChanged() {
+		Cat cat = new Cat("Tom",4,"Black");
+		cat.setColour("White");
+		assertEquals("White",cat.getColour());
+	}
+}
