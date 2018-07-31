8a1f36af3469ff3f936cdb0d4ffc1821ba0b321e
Joe O'Regan <joeaoregan@yahoo.ie>
2018-07-31 08:38:49 +0100

    Change packages

---
+++ src/com/ait/wk5/e5A/Rock2.java
@@ -0,0 +1,31 @@
+/*
+ * Joe O'Regan
+ * A00258304
+ * 30/07/2018
+ * Lecture 5 OOP Notes
+ */
+package com.ait.wk5.e5A;
+
+public class Rock2 {
+	private static int numRocks;// belongs to class
+	private int rockCode;// assign a code
+
+	public Rock2(int rockCode) {
+		this.rockCode = rockCode;
+		numRocks++;
+		System.out.println("Creating Rock...");
+	}
+
+	public int getRockCode() {
+		return rockCode;
+	}
+
+	public static int getNumRocks() {
+		return numRocks;
+	}
+	
+	@Override
+	public String toString() {
+		return ("Code is " + rockCode);
+	}
+}
---
+++ src/com/ait/wk6/UML/BusDepot/BusDepot.java
@@ -0,0 +1,35 @@
+/*
+ * Joe O'Regan
+ * A00258304
+ * 30/07/2018
+ * Java Lab: 5 Bus Depot
+ */
+package com.ait.wk6.UML.BusDepot;
+
+public class BusDepot {
+
+	public static void main(String[] args) {
+		Driver driverOne = new Driver();
+		driverOne.setName("John");
+		driverOne.setPhoneNumber(110);
+
+		Bus busOne = new Bus();
+		busOne.setMake("Volvo");
+		busOne.setNumberOfSeats(45);
+		busOne.setDriver(driverOne);
+
+		System.out.println(busOne);
+
+		Bus busTwo = new Bus();
+		busTwo.setMake("Mercedes");
+		busTwo.setNumberOfSeats(50);
+
+		System.out.println(busTwo);
+
+		Driver driverTwo = new Driver("Tom", 999);
+		Bus busThree = new Bus("Ford", 20, driverTwo);
+
+		System.out.println(busThree);
+	}
+
+}
